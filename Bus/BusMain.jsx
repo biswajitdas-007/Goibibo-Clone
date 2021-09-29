f08bf8189e360251f64397af89bcb7b1f2bd982c
piyush <piyushroxxxx425@gmail.com>
2021-09-29 16:49:20 +0530

    search destination

---
+++ Bus/BusMain.jsx
@@ -0,0 +1,15 @@
+import { SearchBus } from "./SearchBus"
+export function BusMain() {
+    return (
+        <div
+        style={{fontSize:"medium"}}>
+            
+              <SearchBus/>
+            
+            
+            
+
+        </div>
+    )
+    
+}
