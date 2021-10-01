dde073604d1c648cc2be6aa48af819a3bbd2ca40
gsunil1996 <sunil_nj2_085@masai.school>
2021-10-01 21:38:55 +0530

    first commit

---
+++ goibibo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Bus from './Components/Bus/Bus';
+
+function App() {
+  return (
+    <div className="App">
+    <Bus />
+    </div>
+  );
+}
+
+export default App;
