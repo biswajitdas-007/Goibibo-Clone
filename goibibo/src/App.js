15bb3ef6506ba32156856ec225ceab63cb53fd83
Pranali-5 <malkarpg@gmail.com>
2021-10-02 15:14:49 +0530

    trial payment page

--- goibibo/src/App.js
+++ goibibo/src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
-import Bus from './Components/Bus/Bus';
-import { Payment } from "./Components/Payment/Payment";
+import "./App.css";
+import Bus from "./components/Bus/Bus";
+import { Payment } from "./components/Payment/Payment";
 import { Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      
       <Route exact path="/">
         <Bus />
       </Route>
