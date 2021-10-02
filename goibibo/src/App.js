6055c11314bc7ec3070cd489044abf99ad023dcf
Pranali-5 <malkarpg@gmail.com>
2021-10-02 22:03:25 +0530

    log.svg deleted

--- goibibo/src/App.js
+++ goibibo/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Bus from './Components/Bus/Bus';
+import "./App.css";
+import Bus from "./Components/Bus/Bus";
 
 function App() {
   return (
     <div className="App">
-    <Bus />
+      <Bus />
     </div>
   );
 }
